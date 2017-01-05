8b75d73764fee057a4bd8df13fa5475115ef2c54
longshao <longshao@longshao-PC>
2017-01-05 09:52:27 +0800

    sss

--- app/src/main/java/com/like/longshao/CdLongApplication.java
+++ app/src/main/java/com/like/longshao/CdLongApplication.java
@@ -3,7 +3,7 @@ package com.like.longshao;
 import com.like.longshaolib.CdBaseApplication;
 
 /**
- * Created by longshao on 2016/9/27.
+ * Created by longshao on 2016/9/27.  sss
  */
 
 public class CdLongApplication extends CdBaseApplication{
